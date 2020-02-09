6163b1dc466d48867c6ca3de30fc250aaee0ff00
smdhotre <smdhotre123@gmail.com>
2020-02-10 00:05:49 +0530

    feat(quiz): color quiz swings the right one

--- src/pages/ColorsQuiz/child.js
+++ src/pages/ColorsQuiz/child.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { quizList } from './data';
+import { swing } from 'react-animations';
+import styled, { keyframes } from 'styled-components';
+
+const swingAnimation = keyframes`${swing}`;
+
+const SwingDiv = styled.div`
+  animation: 1s ${swingAnimation};
+`;
 
 class Child extends Component {
   constructor(props) {
     super(props)
-    this.state = { hidden: "" };
+    this.state = { hidden: "", swingTheRightOne: false };
   }
   componentWillMount() {
     var that = this;
     setTimeout(function () {
-      that.hide();
+      that.swing();
+      setTimeout(function () {
+        that.hide();
+      }, 2000)
     }, that.props.wait);
+  }
+  swing() {
+    this.setState({ swingTheRightOne: true })
   }
   hide() {
     this.setState({ hidden: "hidden" }, () => {
@@ -33,9 +47,15 @@ class Child extends Component {
             <div className="title">{quizList[this.props.color].images[this.props.index].name}</div>
           </div>
           <div className="quiz-options">
-            {optionList.map(opt => (
-              <div style={{ backgroundColor: opt }} className="option"></div>
-            ))}
+            {optionList.map(opt => {
+              if (this.state.swingTheRightOne
+                && quizList[this.props.color].images[this.props.index].color === opt) {
+                return <SwingDiv style={{ backgroundColor: opt }} className="option"></SwingDiv>
+              }
+              return (
+                <div style={{ backgroundColor: opt }} className="option"></div>
+              )
+            })}
           </div>
         </div>
       </div>
