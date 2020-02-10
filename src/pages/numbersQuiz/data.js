d07161991932410fce8d22cdc84f2278b5464e17
smdhotre <smdhotre123@gmail.com>
2020-02-10 17:56:05 +0530

    feat(quiz): marathi number quiz implementation done

---
+++ src/pages/numbersQuiz/data.js
@@ -0,0 +1,12 @@
+export const quizList = [
+  { id: 1, title: "१", source: require('./images/1.jpg') },
+  { id: 2, title: "२", source: require('./images/2.jpg') },
+  { id: 3, title: "३", source: require('./images/3.jpg') },
+  { id: 4, title: "४", source: require('./images/4.jpg') },
+  { id: 5, title: "५", source: require('./images/5.jpg') },
+  { id: 6, title: "६", source: require('./images/6.jpg') },
+  { id: 7, title: "७", source: require('./images/7.jpg') },
+  { id: 8, title: "८", source: require('./images/8.jpg') },
+  { id: 9, title: "९", source: require('./images/9.jpg') },
+  { id: 10, title: "१०", source: require('./images/10.jpg') }
+]
--- src/pages/numbersQuiz/index.js
+++ src/pages/numbersQuiz/index.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import { quizList } from './data';
+import Child from './child';
 // import numbers from './numbers';
 
 class Numbers extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+      rerenderChild: true,
+      indexes: quizList
+    }
+  }
+
+  increment = () => {
+    const i = this.randomInt(0, 9 - this.state.count - 1);
+    var ids = this.state.indexes.filter((id, index) => (index !== i));
+    const obj = this.state.indexes[i];
+    ids.push(obj);
+    this.setState({ count: this.state.count + 1, indexes: ids, rerenderChild: false, 
+      // x: obj.x, y: obj.y 
+    }, () => {
+      if (this.state.count < 8) {
+        this.setState({ rerenderChild: true })
+      }
+    })
+  }
+
+  randomInt = (min, max) => {
+    return min + Math.floor((max - min) * Math.random());
+  }
+
+  getOptions = () => {
+    let optList = quizList;
+    let opts = [];
+    for (let index = 0; index < 4; index++) {
+      let i = this.randomInt(0, 9 - opts.length);
+      opts.push(optList[i]);
+      optList = optList.filter(opt => opt !== opts[opts.length - 1])
+      optList.push(opts[opts.length - 1])
+    }
+    return opts;
+  }
+
   render() {
     return (
       <div className="App">
         <header className="number-header">
           <div className="welcome-text">अंक प्रश्नोत्तरी</div>
         </header>
+        {this.state.rerenderChild
+          && <Child
+            wait={5000}
+            color={this.randomInt(0, 9)}
+            // index={this.randomInt(0, 2)}
+            increment={this.increment}
+            options={this.getOptions()}
+            offset={this.randomInt(0, 3)}
+          />
+        }
       </div>
     );
   }
