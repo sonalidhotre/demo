1c5c1bfd24e80744f70c0a7f9c69d131faa25ffc
smdhotre <smdhotre123@gmail.com>
2020-02-11 00:47:10 +0530

    fix(bricks): index and quiz bricks style

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -6,30 +6,27 @@ class Quiz extends Component {
   }
   
   render() {
+    const indexes = ["रंग प्रश्नोत्तरी", "आकार प्रश्नोत्तरी", "मुळाक्षरे प्रश्नोत्तरी", "अंक प्रश्नोत्तरी", "अल्फाबेट्स प्रश्नोत्तरी", "नंबर प्रश्नोत्तरी"];
     return (
       <div className="App">
         <header className="quiz-header">
           <div className="quiz-text">प्रश्नोत्तरी ( Quiz )</div>
         </header>
         <div className="index-wrapper">
-          <div className="color-title" onClick={() => this.showPage('colorsQuiz')}>
-            <div className="title">रंग प्रश्नोत्तरी</div>
-          </div>
-          <div className="shape-title" onClick={() => this.showPage('shapesQuiz')}>
-            <div className="title">आकार प्रश्नोत्तरी</div>
-          </div>
-          <div className="alpha-title" onClick={() => this.showPage('alphabetsQuiz')}>
-            <div className="title">मुळाक्षरे प्रश्नोत्तरी</div>
-          </div>
-          <div className="number-title" onClick={() => this.showPage('numbersQuiz')}>
-            <div className="title">अंक प्रश्नोत्तरी</div>
-          </div>
-          <div className="alphabets-title" onClick={() => this.showPage('engAlphaQuiz')}>
-            <div className="title">Alphabets (अल्फाबेट्स - इंग्रजी अक्षरे) Quiz</div>
-          </div>
-          <div className="nums-title" onClick={() => this.showPage('engNumbersQuiz')}>
-            <div className="title">Numbers (नंबर - अंक)</div>
-          </div>
+        {indexes.map((content, index) => {
+            if (index % 2 === 0) {
+              return (
+                <div className="index-odd-title" onClick={() => this.showPage('shapes')}>
+                  <div className="title">{content}</div>
+                </div>
+              )
+            }
+            return (
+              <div className="index-even-title" onClick={() => this.showPage('colors')}>
+                <div className="title">{content}</div>
+              </div>
+            )
+          })}
         </div>
       </div>
     )
